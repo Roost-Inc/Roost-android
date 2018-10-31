9e3fe58af9827a2f84fa9883e3206589a6f55869
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-10-31 09:08:18 +0200

    add transition between two activites

--- app/src/main/java/com/vacuum/roost/ui/main/MainActivity.kt
+++ app/src/main/java/com/vacuum/roost/ui/main/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.vacuum.roost.ui.main
 
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.vacuum.roost.R
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        fun getStartIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,4 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+
+
 }
