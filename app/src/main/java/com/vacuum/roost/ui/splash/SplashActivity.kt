e292a35b6b500b75c51da230004ae2e43a35b4a7
Mohamed Ebrahim <ebrahimm131@gmail.com>
2018-10-31 08:58:16 +0200

    SplashMvpPresenter interface

--- app/src/main/java/com/vacuum/roost/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/vacuum/roost/ui/splash/SplashActivity.kt
@@ -8,7 +8,7 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
 
 
     }
